ccdedb7ae30c0bcefa746fb1d723a5091160c5b1
mscharlock <michelle.scharlock@gmail.com>
2017-10-23 14:31:51 -0700

    we updated the group model

---
+++ src/model/group.js
@@ -0,0 +1,45 @@
+import createError from 'http-errors';
+import * as util from '../lib/util.js';
+import Mongoose, {Schema} from 'mongoose';
+
+const groupSchema = new Schema({
+  owner: {type: Schema.Types.ObjectId, required: true, unique: true},
+  groupName: {type: String, required: true},
+  profiles: {type: Array},
+  description: {type: String},
+});
+
+const Group = Mongoose.model('group', groupSchema);
+
+Group.create = function(req){
+  return new Group({
+    owner: req.body._id,
+    groupName: req.body.groupName,
+    profiles: req.body.profiles,
+    description: req.body.description,
+  }).then(profile => {
+    req.group = req.body._id;
+    return req.group.save()
+    .then(() => profile);
+  });
+};
+
+Group.fetchOne = function(req){
+  return Group.findById(req.params.id)
+    .then(group => {
+      if(!group)
+        throw createError(404, 'NOT FOUND ERROR: group not found');
+      return group;
+    });
+};
+
+Group.update = function(req){
+  let options = {new: true, runValidators: true};
+  return Group.findByIdAndUpdate(req.params.id, {description: req.body.description, profiles: req.body.profiles, groupName: req.body.groupName}, options);
+};
+
+Group.delete = function(req){
+  return Group.findOneAndRemove({_id: req.params.id, owner: req.user._id});
+};
+
+export default Group;
